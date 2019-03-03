a4bf817ccb76bfb9ec4fe0cc46fea290b1e4e979
aaronc13 <adcmyb@mst.edu>
2019-03-02 21:30:58 -0600

    Trying to get categories

--- static/script.js
+++ static/script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
 
   $(".fa-search").click(function(){
@@ -5,10 +6,4 @@ $(document).ready(function(){
     $("input[type='text']").focus();
   });
 
-});
-
-$.ajax({
-  url: "https://developers.zomato.com/api/v2.1/categories",
-  headers: {"user-key": "8fadb61c1f4968f4a79b989c2776f9ae"},
-  complete: function(data){console.log(data)}
 });
