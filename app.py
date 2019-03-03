b0365e205d6979246d114bd583f4b20951292d97
aaronc13 <adcmyb@mst.edu>
2019-03-02 19:25:23 -0600

    updated for multiple files

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template, request, g, send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
-# def hello():
-#     return "Hello World!"
 def index():
     return render_template('DinO.html')
+
+@app.route('/search')
+def search():
+	return render_template('search.html')
 
 if __name__ == '__main__':
     app.run()
